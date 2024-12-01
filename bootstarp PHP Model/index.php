60498b010e2bc1ff1929ad384c312ec3ebae742f
Sudhir Kumar <sudho712@gmail.com>
2024-12-01 20:23:55 +0530

    add form model

--- bootstarp PHP Model/index.php
+++ bootstarp PHP Model/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,10 +11,53 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
+
 <body>
-    <div class="container">
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="completeModel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- Add Form detail  -->
+                    <div class="mb-3">
+                        <label for="completeName" class="form-label">Full Name</label>
+                        <input type="text" class="form-control" id="completeName" placeholder="Enter your Name">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="completeEmail" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="completeName" placeholder="Enter your Email">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="completeMobile" class="form-label">Mobile</label>
+                        <input type="number" class="form-control" id="completeMobile" placeholder="Enter your Mobile">
+                    </div>
+                    <div class="mb-3">
+                        <label for="completeName" class="form-label">Place</label>
+                        <input type="text" class="form-control" id="completeName" placeholder="Enter your Place">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container my-3">
         <h1 class="text-center">PHP CRUD Operation using bootstrap Model</h1>
-        <button class="btn btn-dark">Add New User</button>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#completeModel">
+            Add New User
+        </button>
     </div>
 </body>
+
 </html>
