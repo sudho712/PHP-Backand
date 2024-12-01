4b333eb9afa22e067e7fe94329ec5bda01ace947
Sudhir Kumar <sudho712@gmail.com>
2024-12-01 20:04:42 +0530

    start bootstrap php model

---
+++ bootstarp PHP Model/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php bootstrap model CURD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">PHP CRUD Operation using bootstrap Model</h1>
+        <button class="btn btn-dark">Add New User</button>
+    </div>
+</body>
+</html>
